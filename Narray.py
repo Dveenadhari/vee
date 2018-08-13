6db1c78fa4a53c9c5811e2725d455cc9aeb44a75
Dveenadhari <41769824+Dveenadhari@users.noreply.github.com>
2018-08-13 11:19:49 +0530

    Create Narray.py

---
+++ Narray.py
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,n,max;
+printf("enter the range");
+scanf("%d",&n);
+print("enter the array value");
+for(i=0;i<n;i++);
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("%d",max);
+}
