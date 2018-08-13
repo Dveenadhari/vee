8018c052dc21f7d32c122f2496d2c304f1b9b12d
Dveenadhari <41769824+Dveenadhari@users.noreply.github.com>
2018-08-13 11:45:04 +0530

    Create N interger.py

---
+++ N interger.py
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a[10],temp;
+int i;
+for(i=o;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&a[i]);
+}
+temp=[0];
+for(i=0;i<10;i++)
+{
+if(a[0]>a[i])
+{
+temp=a[i];
+}
+printf("the largest number is %d",temp);
+}
+}
